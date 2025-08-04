b1e1278e94d4289e89ace83ed5161ec65e0b1904
Rennan Rodrigues <rennan.rrm@gmail.com>
2025-08-04 17:21:21 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -24,3 +24,63 @@ int main() {
 
     Carta cartas[TOTAL_CARTAS] = {
         {"Dragão Flamejante", 90, 60, 40},
+        {"Cavaleiro Negro", 70, 80, 60},
+        {"Mago Ancião", 50, 40, 95}
+    };
+
+    // Sorteia carta do jogador e do computador
+    int jogadorIndex = rand() % TOTAL_CARTAS;
+    int pcIndex;
+    do {
+        pcIndex = rand() % TOTAL_CARTAS;
+    } while (pcIndex == jogadorIndex); // evita pegar a mesma carta
+
+    Carta jogador = cartas[jogadorIndex];
+    Carta computador = cartas[pcIndex];
+
+    printf("=== SUA CARTA ===\n");
+    mostrarCarta(jogador);
+
+    // Jogador escolhe atributo
+    int escolha;
+    printf("\nEscolha um atributo para batalhar:\n");
+    printf("1 - Força\n");
+    printf("2 - Velocidade\n");
+    printf("3 - Inteligência\n");
+    printf("Digite sua escolha: ");
+    scanf("%d", &escolha);
+
+    int valorJogador = 0, valorPC = 0;
+    switch (escolha) {
+        case 1:
+            valorJogador = jogador.forca;
+            valorPC = computador.forca;
+            break;
+        case 2:
+            valorJogador = jogador.velocidade;
+            valorPC = computador.velocidade;
+            break;
+        case 3:
+            valorJogador = jogador.inteligencia;
+            valorPC = computador.inteligencia;
+            break;
+        default:
+            printf("Escolha inválida!\n");
+            return 1;
+    }
+
+    printf("\n=== CARTA DO COMPUTADOR ===\n");
+    mostrarCarta(computador);
+
+    printf("\nSeu valor: %d x %d :Computador\n", valorJogador, valorPC);
+
+    if (valorJogador > valorPC) {
+        printf("🎉 Você venceu!\n");
+    } else if (valorJogador < valorPC) {
+        printf("💀 Você perdeu!\n");
+    } else {
+        printf("🤝 Empate!\n");
+    }
+
+    return 0;
+}
